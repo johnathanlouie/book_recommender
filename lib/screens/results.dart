92820d3a7c944fa9284789d555dbb6ef1b51e3d0
Johnathan Louie <johnathanlouie88@gmail.com>
2022-01-29 04:41:29 -0800

    Implement results screen.

--- lib/screens/results.dart
+++ lib/screens/results.dart
@@ -1,5 +1,7 @@
+import 'package:book_recommender/common.dart';
 import 'package:book_recommender/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ResultsScreen extends StatefulWidget {
   const ResultsScreen({Key? key}) : super(key: key);
@@ -12,6 +14,38 @@ class _ResultsScreenState extends State<ResultsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Search Results')),
+      body: Consumer<SearchResults>(
+        builder: (context, results, child) {
+          return ListView.builder(
+            itemCount: results.length,
+            itemBuilder: (context, index) {
+              Book book = results.books[index];
+              return ListTile(
+                leading: Image.network(book.thumbnail),
+                title: Text(book.title),
+                subtitle: Text(book.description),
+                trailing: Consumer<BookCollection>(
+                  builder: (context, books, child2) {
+                    return IconButton(
+                      icon: books.contains(book)
+                          ? const Icon(Icons.star)
+                          : const Icon(Icons.star_border_outlined),
+                      onPressed: () {
+                        if (books.contains(book)) {
+                          books.removeBook(book.googleId);
+                        } else {
+                          books.addBook(book);
+                        }
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+          );
+        },
+      ),
       bottomNavigationBar: const MyBottomBar(MyBottomBar.results),
     );
   }
