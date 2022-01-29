70d4ec0995c53307760a56c59afddc8a8e70e4c1
Johnathan Louie <johnathanlouie88@gmail.com>
2022-01-29 01:00:51 -0800

    Add log out button.

--- lib/screens/account.dart
+++ lib/screens/account.dart
@@ -1,5 +1,8 @@
+import 'package:book_recommender/common.dart' as common;
 import 'package:book_recommender/widgets/bottom_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({Key? key}) : super(key: key);
@@ -12,6 +15,21 @@ class _AccountScreenState extends State<AccountScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Account')),
+      body: Consumer<common.User>(builder: (context, user, child) {
+        return ElevatedButton(
+          onPressed: () async {
+            try {
+              FirebaseAuth.instance.signOut();
+              user.logOut();
+            } catch (e) {
+              // TODO: Add real error handling.
+              print(e);
+            }
+          },
+          child: const Text('Sign Out'),
+        );
+      }),
       bottomNavigationBar: const MyBottomBar(MyBottomBar.account),
     );
   }
